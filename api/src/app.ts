4d714a95d15a0c90a80e820954867a722bd660ad
adr1an <adrn.cederberg123@gmail.com>
2021-10-05 11:01:56 -0600

    Messing around with an express to decide exactly how to approach building endpoints.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,20 +1,49 @@
 import create_app from './server'
 import create_db from './db'
-import { content_model as content } from './model/model'
-import { ContentInterface as Content } from './model/types'
+import models from './model/model'
 
 const app = create_app()
 const db = create_db()
 
 const markdown = '/markdown/'
+const collection = 'collection'
+
+const get_model = ( request ) => models[ request.body.collection ]
+const get_err = ( err, result ) => result.status( 400 ) && result.send( err ) 
+const has_key = ( key, request, result ) => !request.body.hasOwnProperty( '_id' ) && get_err( 'InsufficientKeys', result )
 
 app.post(markdown, async ( request, result ) => {
-	const data = await content.create( request.body )
-	console.log( data )
-	result.send('It works!')
+	const model = await get_model( request )
+	if ( !model ){ get_err( "ModuleNotFound", result ) }
+
+	const data = await get_model( request ).create( request.body )
+	result.send( data )
 } )
 app.get(markdown, async ( request, result ) => {
-	const data = await content.find( request.body )
-	console.log( data )
-	result.send('This also works!')
+	// Otherwise, find the data in the specific collection with the specific filtering statements.
+	// If fails, get_err will handel it and return the error.
+	const model = await get_model( request )
+	if ( !model ){ 
+		get_err( "ModuleNotFound", result ) 
+		return
+	}
+	console.log( model )	
+	model.find( request.body )
+	.then( data => result.send( data ) )
+	.catch( err => get_err( err, result ) )
+} )
+app.delete( markdown, async ( request, result ) => {
+	// Should only be able to delete from id.
+	// Does not take filtering statements or any other parameters.
+	
+	const model = await get_model( request )
+	if ( !model ){ 
+		get_err( "ModuleNotFound", result ) 
+		return
+	}
+
+	model.find( request.body )
+	.then( data => { data.map( item => item.delete() ); return data } ) 
+	.then( data => { result.send( data ) } )
+	.catch( err => get_err( err, result ) )
 } )
--- api/src/model/model.ts
+++ api/src/model/model.ts
@@ -3,6 +3,11 @@ import { model } from 'mongoose'
 import { content_schema } from './schema'
 import { ContentDocument } from './types'
 
-export const content_model = model<ContentDocument>( "Session", content_schema )
+// Exports a function so that we make create models for the individual collections of documents.
+export function content_model( name : string ){ model<ContentDocument>( name, content_schema ) }
 
+export default {
+       	tests : content_model( 'tests' ),
+	python : content_model( 'python' )
+}
 
