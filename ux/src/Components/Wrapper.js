6541fa9438c441cc1ca63f9e0b9712a0bece1d50
adr1an <adrn.cederberg123@gmail.com>
2021-10-12 18:02:00 -0600

    Messed up and deleted the tool for posting dummy data

--- ux/src/Components/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import './Wrapper.css';
-import Navbar from './Navbar.js'
-
-export default function Wrapper(props) { return ( <>
-	<Navbar/>
-	<div className = "bg-light text-dark Wrapper" { ...props } ></div>
-	</>
-) }
