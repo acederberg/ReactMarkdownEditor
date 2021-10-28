75b63744aaf94068ee5c0db7be6e79cdf3226444
adr1an <adrn.cederberg123@gmail.com>
2021-10-28 12:23:03 -0600

    moved authentication stuff to auth, re-wrote import statements.

---
+++ ui/src/components/index.js
@@ -0,0 +1,17 @@
+import Collections from './collection'
+import View from './view.js'
+import Edit from './edit.js'
+import Navbar from './navbar.js'
+import { ViewerContextProvider } from "./viewerContext.js"
+import AuthProviderWithHistory from "./auth/AuthProviderWithHistory.js"
+import ProtectedRoute from "./auth/protectedRoute.js"
+
+export { 
+	Collections, 
+	View, 
+	Edit, 
+	Navbar,
+	ViewerContextProvider,
+	AuthProviderWithHistory,
+	ProtectedRoute,
+};
