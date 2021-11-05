0ca287414911cdd2a830d32ccc68e7ff67df2b4f
adr1an <adrn.cederberg123@gmail.com>
2021-11-05 10:54:02 -0600

    Added the TESTING environment variable to make sure that tokens aren't needlessly spent during development. Just export TESTING=false to run it back on. Auth middleware is replaced with trivial middleware.

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,7 +6,11 @@ var jwt = require('express-jwt');
 var jwtAuthz = require('express-jwt-authz')
 var jwks = require('jwks-rsa');
 
-export const checkUser = jwt({
+// Use trivial middleware to avoid using up tokens. 
+const TESTING = ( process.env.TESTING === 'true' )
+const testing_middleware = ( request, response, next ) => next()
+
+const checkUser =  !TESTING ? jwt({
 	secret: jwks.expressJwtSecret({
 		cache: true,
 		rateLimit: true,
@@ -16,13 +20,14 @@ export const checkUser = jwt({
 	audience: process.env.REACT_APP_TOKEN_AUDIENCE,
 	issuer: process.env.REACT_APP_TOKEN_ISSUER,
 	algorithms: ['RS256']
-});
+}) : testing_middleware
 
-export const checkAdmin = jwtAuthz(
+const checkAdmin = !TESTING ? jwtAuthz(
 	[ "modify:articals" ], 
 	{ customScopeKey : "permissions" }
-)
+) : testing_middleware
 
+export { checkUser, checkAdmin }
 export default function create_app(){
 	const port = process.env.PORT ? process.env.PORT : 8000
 	const app = express()
