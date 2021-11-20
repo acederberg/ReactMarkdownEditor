25587c276b61cdc8c35184213f93ae0099d595b4
adr1an <adrn.cederberg123@gmail.com>
2021-11-20 13:15:25 -0700

    All docker steps no longer use shell scripts (except for checking steps) and work as anticipated.

--- ansible/vagrant/add_ansible.sh
+++ ansible/vagrant/add_ansible.sh
@@ -1,5 +1,8 @@
+# Make the ansible users home directory.
 mkdir /home/ansible 
-useradd -d /home/ansible ansible
+useradd -G sudo -d /home/ansible ansible
+# Set correct ownership.
 chown ansible /home/ansible && chgrp ansible /home/ansible
+# Set password, add to sudoers file.
 echo 'ansible:ansible' | chpasswd
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
