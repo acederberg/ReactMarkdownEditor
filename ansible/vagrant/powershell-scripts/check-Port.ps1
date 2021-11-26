f78287e590f40653a9d98b7e63a6976f5c001525
adr1an <adrn.cederberg123@gmail.com>
2021-11-26 12:13:40 -0700

    Fixed check-port issues with write-verbose. Fixed a naming issue. Fixed not using inpu./

--- ansible/vagrant/powershell-scripts/check-Port.ps1
+++ ansible/vagrant/powershell-scripts/check-Port.ps1
@@ -1,31 +1,31 @@
 param (
-	$port
+	$PORT
 )
 
-if ( $port -eq $null ){ throw "You must specify a port." }
+if ( $PORT -eq $null ){ throw "You must specify a port." }
 
-$port = get-NetTCPConnection | where-Object{ 
-	$_.LocalPort -eq $port 
+$localport = Get-NetTCPConnection | where-Object{ 
+	$_.LocalPort -eq $PORT 
 }
 
+if ( $localport ) {
 
-if ( $port -eq $null )
-{
-	Write-Verbose ( 'port={0} is free to use.' -f $port )
-}
-else {
 	# List port 4343 details and the ascociated process.
 	# https://azega.org/list-open-ports-using-powershell/
 
 	$with_process = Get-NetTCPConnection `
-		|	where {( $_.LocalPort -eq 4343 )} `
+		|	where {( $_.LocalPort -eq $PORT )} `
 		| select LocalAdress,LocalPort,RemoteAdress,RemotePort,State,AppliedSetting,OwningProcess,@{
 				Name = "Process";
 				Expression={
 					( Get-Process -Id $_.OwningProcess ).ProcessName
 				};
 		} | ft
-	Write-Verbose ( 'port={0} is not free, here is some useful information:' -f $port )
+	echo ( 'port={0} is not free, here is some useful information:' -f $PORT )
 	echo $with_process
+
+} else {
+	
+	echo 'port is open for use.'
 
 }
