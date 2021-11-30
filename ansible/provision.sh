c6657b3fbed52141b86fd40b07f8643b1e4a315b
adr1an <adrn.cederberg123@gmail.com>
2021-11-30 15:21:01 -0700

    various fixes to ansible-runner (in vagrant ) provisioning

---
+++ ansible/provision.sh
@@ -0,0 +1,5 @@
+./vagrant/add_ansible.sh
+
+apt install ansible
+ansible-galaxy collection install community.docker
+
--- ansible/vagrant/add_ansible.sh
+++ ansible/vagrant/add_ansible.sh
@@ -1,6 +1,6 @@
 # Make the ansible users home directory.
 mkdir /home/ansible 
-useradd -G sudo -d /home/ansible ansible
+useradd -G sudo -d /home/ansible ansible --shell /bin/bash
 # Set correct ownership.
 chown ansible /home/ansible && chgrp ansible /home/ansible
 # Set password, add to sudoers file.
