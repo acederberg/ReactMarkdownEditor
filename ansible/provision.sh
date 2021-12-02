020ca9e380f780f67006c85d761946e0824b1d27
adr1an <adrn.cederberg123@gmail.com>
2021-12-02 16:52:01 -0700

    Moved the backup role into the roles folder. Added variables to define sources and targets. Wrote tasks as loops. Works. Next will add tasks to the app runer role to push these backups when they do not exist.

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -1,5 +1,6 @@
 ./vagrant/add_ansible.sh
 
 apt install ansible
-ansible-galaxy collection install community.docker
+ansible-galaxy collection install community.docker 
+ansible-galaxy collection install community.general
 
