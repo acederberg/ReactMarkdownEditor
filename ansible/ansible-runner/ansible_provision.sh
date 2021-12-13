d8f48f958f08ce35eb834ebed74ae30f36e7dc21
adr1an <adrn.cederberg123@gmail.com>
2021-12-13 12:26:13 -0700

    Fixed error in ansible-runner provisioning script.

--- ansible/ansible-runner/ansible_provision.sh
+++ ansible/ansible-runner/ansible_provision.sh
@@ -1,4 +1,6 @@
-./vagrant/add_ansible.sh
+test -d ansible && mkdir /home/ansible
+useradd ansible -d /home/ansible -g sudo
+chown ansible:ansible /home/ansible
 
 apt install ansible
 ansible-galaxy collection install community.docker 
