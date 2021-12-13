129cfd0e4b240d2d40f883de5c7f42cbc55bff2a
adr1an <adrn.cederberg123@gmail.com>
2021-12-13 11:27:11 -0700

    Removed rebuild shell script since it sucks and since ps is cross platform. Renamed powershell-scripts to ps.

--- ansible/vagrant/rebuild.ps1
+++
@@ -1,9 +0,0 @@
-param ( 
-	$vagrant,
-	$mode,
-	$SSHKeys
-)
-
-import-Module './powershell-scripts/rebuild.local.ps1' -force
-
-rebuild-LocalAppRunnerEnvironment -vagrant $vagrant -mode $mode -SSHKeys $SSHKeys
